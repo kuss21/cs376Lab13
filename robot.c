7428d9fc013efcace91d46ab3d617fe309e2418b
Kuss <kuss21@egr-linux-8590.campus.up.edu>
2020-04-16 20:34:14 -0700

    Animation commit.

--- robot.c
+++ robot.c
@@ -33,14 +33,32 @@ void robotPrintAscii(void)
   int i;
   char* blank = "                              ";
   blank = blank +30;
-  printf("\n\n"
+  for(i=0;i<3;i++){
+
+    printf("\n\n"
 	 "UWR: Unidentified Wheeled Robot\n"
 	 "%s    i_i    \n"
 	 "%s   [@_@]   \n"
 	 "%s  /|___|\\ \n"
 	 "%s   d   b   \n",blank, blank, blank, blank);
+/*
+	 "%s%s    i_i   \n"
+	 "%s%s   [@_@]  \n"
+	 "%s%s  /|___|\\ \n"
+	 "%s%s   d   b  \n",blank, blank, blank, blank);   
 
+*/
+
+
+
+
+
+
+ 
+
+             
   blank = blank - 10;
+  }
 }
 
 /**
